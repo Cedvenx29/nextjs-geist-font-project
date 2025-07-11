0da89c1e4171178d87b182112eddcfac09ddb647
Cedvenx29 <81857441+Cedvenx29@users.noreply.github.com>
2025-07-12 06:12:30 +0800

    Add CarRentalWPF/DashboardPage.xaml.cs 1752271950076

---
+++ CarRentalWPF/DashboardPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CarRentalWPF
+{
+    public partial class DashboardPage : Page
+    {
+        public DashboardPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
